6d5bacd9a44684c97084c04cd12b020bb06440ed
CancerGuy <70016211+AzhurniyBaklan@users.noreply.github.com>
2021-02-03 17:04:53 +0500

    Add files via upload

--- scripts/units.js
+++ scripts/units.js
@@ -1,3 +1,6 @@
+const surge = extendContent(UnitType, "surge", {});
+surge.constructor = () => extend(UnitEntity, {});
+
 const embarassment = extendContent(UnitType, "embarassment", {});
 embarassment.constructor = () => extend(UnitEntity, {});
 embarassment.defaultController = () => extend(MinerAI, {});
@@ -13,3 +16,4 @@ scarab.defaultController = () => extend(MinerAI, {});
 const enforcer = extendContent(UnitType, "enforcer", {});
 enforcer.constructor = () => extend(UnitEntity, {});
 enforcer.defaultController = () => extend(BuilderAI, {});
+enforcer.abilities.add(new UnitSpawnAbility(surge, 500, 0, -5));
