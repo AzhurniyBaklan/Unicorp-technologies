e12b6d63782485a1f4d2a2ee30d17e186e3f8d56
CancerGuy <70016211+AzhurniyBaklan@users.noreply.github.com>
2021-02-18 22:39:32 +0500

    Add files via upload

--- scripts/units.js
+++ scripts/units.js
@@ -1,5 +1,6 @@
 const surge = extendContent(UnitType, "surge", {});
 surge.constructor = () => extend(UnitEntity, {});
+let blue = Color.valueOf("#e5e5ff");
 
 const embarassment = extendContent(UnitType, "embarassment", {});
 embarassment.constructor = () => extend(UnitEntity, {});
@@ -16,12 +17,16 @@ scarab.defaultController = () => extend(MinerAI, {});
 const enforcer = extendContent(UnitType, "enforcer", {});
 enforcer.constructor = () => extend(UnitEntity, {});
 enforcer.defaultController = () => extend(BuilderAI, {});
-enforcer.abilities.add(new UnitSpawnAbility(surge, 500, 0, -5));
+enforcer.abilities.add(new ForceFieldAbility(70, 7, 220, 600));
+
+const sickle = extendContent(UnitType, "sickle", {});
+sickle.constructor = () => extend(UnitEntity, {});
+sickle.abilities.add(new MoveLightningAbility(25, 10, 0.2, 0.5, 1, 1, blue ));
 
 const razor = extendContent(UnitType, "razor", {});
 razor.constructor = () => extend(UnitEntity, {});
-razor.abilities.add(new ForceFieldAbility(70, 7, 220, 600));
+razor.abilities.add(new  RepairFieldAbility(50, 50, 50));
 
 const aegis = extendContent(UnitType, "aegis", {});
 aegis.constructor = () => extend(UnitEntity, {});
-aegis.abilities.add(new ForceFieldAbility(50, 6, 300, 500));
+aegis.abilities.add(new ForceFieldAbility(70, 6, 300, 300), new ShieldRegenFieldAbility(10, 300, 300, 300));
